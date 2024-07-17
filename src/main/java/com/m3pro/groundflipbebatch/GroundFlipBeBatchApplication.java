4a2210c2d29be5776f46e40b74e141ecaafffde1
MinUk <qjvk2880@gmail.com>
2024-07-17 11:07:27 +0900

    M3-186 Feat : Initial Commit

---
+++ src/main/java/com/m3pro/groundflipbebatch/GroundFlipBeBatchApplication.java
@@ -0,0 +1,13 @@
+package com.m3pro.groundflipbebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroundFlipBeBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroundFlipBeBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/m3pro/groundflipbebatch/GroundFlipBeBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m3pro.groundflipbebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroundFlipBeBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
