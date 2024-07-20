b66b32e9bd0c9e19285baa49f1aaf4f29800314a
MinUk <qjvk2880@gmail.com>
2024-07-20 15:43:15 +0900

    M3-186 Chore : 시간대 변경

--- src/main/java/com/m3pro/groundflipbebatch/GroundFlipBeBatchApplication.java
+++ src/main/java/com/m3pro/groundflipbebatch/GroundFlipBeBatchApplication.java
@@ -1,14 +1,22 @@
 package com.m3pro.groundflipbebatch;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import jakarta.annotation.PostConstruct;
+
 @EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication
 public class GroundFlipBeBatchApplication {
+	@PostConstruct
+	void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GroundFlipBeBatchApplication.class, args);
