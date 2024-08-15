941c71254377e62f9e6c6df801275d80875f5a20
Koo Min <koomin1227@naver.com>
2024-08-15 16:48:16 +0900

    M3-294 Feat : 매일 아침 8시 동의한 유저에게 알림을 보내는 기능 구현

--- src/main/java/com/m3pro/groundflipbebatch/service/BatchService.java
+++ src/main/java/com/m3pro/groundflipbebatch/service/BatchService.java
@@ -11,6 +11,14 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class BatchService {
 	private final RankingService rankingService;
+	private final FcmService fcmService;
+
+	@Scheduled(cron = "0 0 8 * * ?")
+	public void sendDailyWalkNotification() {
+		String title = "\uD83D\uDC5F그라운드 플립과 함께 걸을 시간";
+		String body = "오늘도 그라운드 플립을 켜고 땅을 점령해요!!";
+		fcmService.sendNotificationToAllUsers(title, body);
+	}
 
 	@Scheduled(cron = "0 0 * * * *")
 	public void transferRankingToDatabaseOnEveryMidnight() {
