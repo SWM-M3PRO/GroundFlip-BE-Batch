bf31685e485661b31e468e1bda5e502852fcd152
Koo Min <koomin1227@naver.com>
2024-08-15 16:33:13 +0900

    M3-294 Feat : 서비스 알림을 동의한 유저에게 알림을 보내는 메서드 구현

--- src/main/java/com/m3pro/groundflipbebatch/service/FcmService.java
+++ src/main/java/com/m3pro/groundflipbebatch/service/FcmService.java
@@ -1,5 +1,8 @@
 package com.m3pro.groundflipbebatch.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.google.firebase.messaging.ApnsConfig;
@@ -7,14 +10,41 @@ import com.google.firebase.messaging.Aps;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.MessagingErrorCode;
 import com.google.firebase.messaging.Notification;
+import com.m3pro.groundflipbebatch.entity.FcmToken;
+import com.m3pro.groundflipbebatch.repository.FcmTokenRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class FcmService {
 	private final FirebaseMessaging firebaseMessaging;
+	private final FcmTokenRepository fcmTokenRepository;
+
+	public void sendNotificationToAllUsers(String title, String body) {
+		List<FcmToken> fcmTokens = fcmTokenRepository.findAllTokensForServiceNotifications();
+		List<Long> failedTokensId = new ArrayList<>();
+
+		fcmTokens.forEach(fcmToken -> {
+			try {
+				log.info(fcmToken.getId().toString());
+				sendMessage(title, body, fcmToken.getToken());
+			} catch(FirebaseMessagingException e) {
+				if (isInvalidTokenError(e)) {
+					failedTokensId.add(fcmToken.getId());
+					log.warn("Failed to send notification to [{}]: {}", fcmToken.getUser().getId(), fcmToken.getToken());
+				}
+			}
+		});
+
+		if (!failedTokensId.isEmpty()) {
+			fcmTokenRepository.deleteAllById(failedTokensId);
+		}
+	}
 
 	public void sendMessage(String title, String body, String fcmToken) throws FirebaseMessagingException {
 		Message message = createMessage(title, body, fcmToken);
@@ -43,5 +73,10 @@ public class FcmService {
 				.setSound("default")
 				.build()
 			).build();
+	}
+
+	private boolean isInvalidTokenError(FirebaseMessagingException e) {
+		MessagingErrorCode errorCode = e.getMessagingErrorCode();
+		return errorCode.equals(MessagingErrorCode.UNREGISTERED) || errorCode.equals(MessagingErrorCode.INVALID_ARGUMENT);
 	}
 }
