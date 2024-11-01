bd924d2dae8e92b55d14cd238c222481160c0f3a
Koo Min <koomin1227@naver.com>
2024-11-01 13:53:27 +0900

    M3-416 Feat : 상위 100 등의 유저 조회

--- src/main/java/com/m3pro/groundflipbebatch/repository/RankingHistoryRepository.java
+++ src/main/java/com/m3pro/groundflipbebatch/repository/RankingHistoryRepository.java
@@ -3,9 +3,18 @@ package com.m3pro.groundflipbebatch.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.m3pro.groundflipbebatch.entity.RankingHistory;
 
 public interface RankingHistoryRepository extends JpaRepository<RankingHistory, Long> {
 	List<RankingHistory> findAllByYearAndWeek(Integer year, Integer week);
+
+	@Query("""
+		SELECT rh FROM RankingHistory rh
+ 		WHERE rh.year = :year AND rh.week = :week
+ 		ORDER BY rh.ranking
+ 		LIMIT 100
+	""")
+	List<RankingHistory> findTop100AllByYearAndWeek(Integer year, Integer week);
 }
